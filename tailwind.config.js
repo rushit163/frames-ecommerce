17d6b7388e36b526a960ce432e6acf390af93d3e
itaintkevin <kpjtofficial@gmail.com>
2023-05-05 18:47:04 +0530

    chore: Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'bg-primary': '#ffffff',
+        'bg-secondary': "#7e57c2"
       },
+      fontFamily: {
+        'primary': ['Playfair Display', 'serif'] 
+      }
     },
   },
   plugins: [],
