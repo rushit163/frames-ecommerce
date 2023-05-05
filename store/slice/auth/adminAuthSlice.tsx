1b3affdfda063a0febe3593fd8274f9728e5ebdd
itaintkevin <kpjtofficial@gmail.com>
2023-05-05 18:24:43 +0530

    chore: Added redux slice to toggle login / signup component

---
+++ store/slice/auth/adminAuthSlice.tsx
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import type { RootState } from '../../store'
+
+// Define a type for the slice state
+interface ToggleComponent {
+  isComponentOn: boolean; 
+}
+
+// Define the initial state using that type
+const initialState: ToggleComponent = {
+  isComponentOn: false,
+}
+
+export const adminAuthSlice = createSlice({
+  name: 'toggleComponent',
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+    toggleComponent: (state) => {
+      state.isComponentOn = !state.isComponentOn;
+    },
+  },
+})
+
+export const { toggleComponent } = adminAuthSlice.actions
+
+// Other code such as selectors can use the imported `RootState` type
+export const toggle = (state: RootState) => state.counter.value
+
+export default adminAuthSlice.reducer
