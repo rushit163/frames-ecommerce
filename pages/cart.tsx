5a15eb9b7758d59bf724e74a576277d49f421af7
Kevin Paul <kpjtofficial@gmail.com>
2023-05-06 15:20:07 +0530

    feat: Added Cart Page and its components

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import Navbar from "@/components/layouts/Navbar";
+import PriceDetails from "@/components/cart/PriceDetails";
+import ProductList from "@/components/cart/ProductList";
 
 export default function cart() {
   return (
-    <div>cart</div>
-  )
+    <main
+      className="flex min-h-screen flex-col gap-10 items-center p-2"
+    >
+      <Navbar />
+
+      <div className="flex flex-col gap-6 items-center p-2">
+        <section className="relative">
+          <ProductList/>
+        </section>
+        <section className="relative">
+          <PriceDetails/>
+        </section>
+      </div>
+    </main>
+  );
 }
