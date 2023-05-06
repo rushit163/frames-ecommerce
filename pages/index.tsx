7d48bf4667fd5f3180fb66abcdf5541843ad3401
itaintkevin <kpjtofficial@gmail.com>
2023-05-06 10:13:22 +0530

    chore: Updating home page styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,8 @@
 import Image from "next/image";
 import Head from "next/head";
-import { Inter } from "next/font/google";
 import Carousel from "@/components/products/Carousel";
 import Products from "@/components/products/Products";
 import Navbar from "@/components/layouts/Navbar";
-
-const inter = Inter({ subsets: ["latin"] });
 
 interface Property {
   images: Array<string>;
@@ -22,13 +19,13 @@ export default function Home({ properties }: Props) {
   return (
     <>
       <Head>
-        <title>Specs</title>
-        <meta name="description" content="" />
+        <title>Framez</title>
+        <meta name="description" content="Framez - Your one-stop destination to find frames that suit your face."/>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main
-        className={`flex min-h-screen flex-col items-center justify-between ${inter.className}`}
+        className="flex min-h-screen flex-col items-center justify-between"
       >
         <Navbar/>
         <Carousel/>
