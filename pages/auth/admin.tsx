267fcdad2695f80d59b61a8b185d72c6c4c3d2ba
thelordzeus <arnav55125@gmail.com>
2023-05-06 22:53:13 +0530

    feat: added admin login page

--- pages/auth/admin.tsx
+++ pages/auth/admin.tsx
@@ -1,7 +1,76 @@
-import React from 'react'
+import React from "react";
 
 export default function admin() {
   return (
-    <div>admin</div>
-  )
+    <section className="bg-white">
+      <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
+        <aside className="relative block h-16 lg:order-last lg:col-span-5 lg:h-full xl:col-span-6">
+          <img
+            alt="Pattern"
+            src="https://images.unsplash.com/photo-1605106702734-205df224ecce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            className="absolute inset-0 h-full w-full object-cover"
+          />
+        </aside>
+
+        <main
+          aria-label="Main"
+          className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6"
+        >
+          <div className="max-w-xl lg:max-w-3xl">
+            <a className="block text-blue-600" href="/">
+              <span className="sr-only">Home</span>
+            </a>
+
+            <h1 className="mt-6 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
+              Welcome to Admin Dashboard
+            </h1>
+
+            <p className="mt-4 leading-relaxed text-gray-500">
+              A place for all your stats and inventory information.
+            </p>
+
+            <form action="#" className="mt-8 grid grid-cols-6 gap-6">
+              <div className="col-span-6">
+                <label
+                  htmlFor="Email"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Email
+                </label>
+
+                <input
+                  type="email"
+                  id="Email"
+                  name="email"
+                  className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
+                />
+              </div>
+
+              <div className="col-span-6">
+                <label
+                  htmlFor="adminPass"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Password
+                </label>
+
+                <input
+                  type="password"
+                  id="adminPass"
+                  name="adminPass"
+                  className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
+                />
+              </div>
+
+              <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
+                <button className="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500">
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
+        </main>
+      </div>
+    </section>
+  );
 }
